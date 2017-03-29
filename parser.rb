28fc22b799aa4f3514c464e026ac7ad4d6a5895d
Lewis John McGibbney <lewis.mcgibbney@gmail.com>
2017-03-29 15:22:29 -0700

    Improve documentation and use new location for SWEET ontologies

--- parser.rb
+++ parser.rb
@@ -4,8 +4,8 @@ require "neography"
 require "active_support/all"
 
 NEO4J_USER = "neo4j"
-NEO4J_PASSWORD = "admin"
-SWEET_URL = "http://sweet.jpl.nasa.gov/2.3/sweetAll.owl"
+NEO4J_PASSWORD = "pass"
+SWEET_URL = "https://raw.githubusercontent.com/ESIPFed/sweet/master/sweet2.3/sweetAll.owl"
 
 class SweetParser
 
