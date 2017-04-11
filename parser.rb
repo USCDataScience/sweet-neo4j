aea24b58be42039b0acb847c47f754127d6982b0
Lewis John McGibbney <lewis.mcgibbney@gmail.com>
2017-04-11 12:08:34 -0700

    Revert dummy pass

--- parser.rb
+++ parser.rb
@@ -4,7 +4,7 @@ require "neography"
 require "active_support/all"
 
 NEO4J_USER = "neo4j"
-NEO4J_PASSWORD = "Sarahp00"
+NEO4J_PASSWORD = "pass"
 SWEET_URL = "https://raw.githubusercontent.com/ESIPFed/sweet/master/2.3/sweetAll.owl"
 
 class SweetParser
